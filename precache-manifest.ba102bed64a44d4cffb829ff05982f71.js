751ca492797129f196cbc0e01689b0276906c969
Kaminsky <kaminskyfrontend@gmail.com>
2021-03-05 05:15:08 +0200

    Updates

--- precache-manifest.ba102bed64a44d4cffb829ff05982f71.js
+++ precache-manifest.ba102bed64a44d4cffb829ff05982f71.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4b07b4a32ef54fffb42869689e4c4cb2",
+    "revision": "8d4a552c054304cf6a05dd6605e75bf5",
     "url": "/online-store/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/online-store/static/css/main.bf87a7ed.chunk.css"
   },
   {
-    "revision": "788ad796cce5befb3905",
-    "url": "/online-store/static/js/2.74fea4cb.chunk.js"
+    "revision": "27af0ff8d832e3e304d6",
+    "url": "/online-store/static/js/2.ec0bc1d4.chunk.js"
   },
   {
     "revision": "1fb1e855e7cec1f1cbdc",
