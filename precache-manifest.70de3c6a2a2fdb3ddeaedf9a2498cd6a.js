c661dfb9ab0cf8c4030e46ec1765fd6f4449dcb3
Kaminsky <kaminskyfrontend@gmail.com>
2021-03-05 05:05:39 +0200

    Updates

---
+++ precache-manifest.70de3c6a2a2fdb3ddeaedf9a2498cd6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b07b4a32ef54fffb42869689e4c4cb2",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "1fb1e855e7cec1f1cbdc",
+    "url": "/online-store/static/css/main.bf87a7ed.chunk.css"
+  },
+  {
+    "revision": "788ad796cce5befb3905",
+    "url": "/online-store/static/js/2.74fea4cb.chunk.js"
+  },
+  {
+    "revision": "1fb1e855e7cec1f1cbdc",
+    "url": "/online-store/static/js/main.6531c7ff.chunk.js"
+  },
+  {
+    "revision": "19e0f7521b8bac23d01b",
+    "url": "/online-store/static/js/runtime~main.ca6cc33d.js"
+  }
+]);
